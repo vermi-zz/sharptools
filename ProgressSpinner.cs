2386be12afff469ac850582bccb03fe4240fbac9
Justin Vermillion <justin@oishiide.su>
2013-09-04 13:30:48 -0400

    Initial Commit

---
+++ ProgressSpinner.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace su.oishiide.tools
+{
+    public class ProgressSpinner
+    {
+        int counter;
+        public ProgressSpinner()
+        {
+            counter = 0;
+        }
+        public void Tick()
+        {
+            counter++;
+            switch (counter % 4)
+            {
+                case 0: Console.Write("/"); break;
+                case 1: Console.Write("-"); break;
+                case 2: Console.Write("\\"); break;
+                case 3: Console.Write("-"); break;
+            }
+            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+        }
+        public void Tick(int x, int y)
+        {
+            counter++;
+            switch (counter % 4)
+            {
+                case 0: Console.Write("/"); break;
+                case 1: Console.Write("-"); break;
+                case 2: Console.Write("\\"); break;
+                case 3: Console.Write("-"); break;
+            }
+            Console.SetCursorPosition(x, y);
+        }
+    }
+}
+
